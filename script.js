fb8d7d6cbd223dcfea01be4413cf85b488799598
Cheryl L <cheryl.loeffler@gmail.com>
2023-03-07 13:57:48 -0700

    added window prompts and captured selected characters in an array

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Character Arrays
-var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var symbols = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{', '}','<','>','?','.',',',':',';'];
-var characterPool = [];
+
+
+//This function gets user preferences, adds them to the character pool array, randomly chooses characters from that array, then calls the check characters function to ensure each type of character is represented and randomly splices in any missing values. 
+
+function generatePassword(){
+    //Character Arrays
+    var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    var symbols = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{', '}','<','>','?','.',',',':',';'];
+    var charPool = [];
+
+    //var numChars = this.prompt("How many characters should your password have?") //this needs to be run through a min/max check at some point.
+    var includeLower = this.confirm("Should your password have lowercase letters? (Y/N)");
+    var includeUpper = this.confirm("Should your password have uppercase letters? (Y/N)");
+    var includeNumbers = this.confirm("Should your password have numbers? (Y/N)");
+    var includeSymbols = this.confirm("Should your password have symbols?");   
+    
+    //this pushes the appropriate characters to an array of all possible characters
+    if (includeLower) {
+        charPool = charPool.concat(lowercase);
+    };
+    if (includeUpper) {
+        charPool = charPool.concat(uppercase);
+    }
+    if (includeNumbers) {
+        charPool = charPool.concat(numbers);
+    }
+    if (includeSymbols) {
+        charPool = charPool.concat(symbols);
+    }
+
+    console.log(charPool);
+    
+    function ensureChars() {
+
+    };
+    function 
+    // ensureChars();
+};
+
+
 
 // Write password to the #password input
 function writePassword() {
